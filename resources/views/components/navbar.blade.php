29b5ce8d3616010fcb19ffe7f47559bdabb3384d
naufalamr17 <naufal.amr17@outlook.co.id>
2023-08-11 14:03:26 +0700

    minor update for UI

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -107,14 +107,6 @@
         'translate-y-0': scrollingUp,
     }"
 >
-    <x-button
-        type="button"
-        icon-only
-        variant="secondary"
-        sr-text="Search"
-    >
-        <x-heroicon-o-search aria-hidden="true" class="w-6 h-6" />
-    </x-button>
 
     <a href="{{ route('dashboard') }}">
         <x-application-logo aria-hidden="true" class="w-10 h-10" />
