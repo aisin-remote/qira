8e2a30ad963ec75011aa7ff26033c6913c022d69
naufalamr17 <naufal.amr17@outlook.co.id>
2023-09-14 10:03:25 +0700

    Update: Fix Profile Dropdown

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,47 +1,37 @@
 <nav aria-label="secondary" x-data="{ open: false }" class="sticky top-0 z-10 flex items-center justify-between px-4 py-4 sm:px-6 transition-transform duration-500 bg-white dark:bg-dark-eval-1" :class="{
-        '-translate-y-full': scrollingDown,
-        'translate-y-0': scrollingUp,
-    }">
+    '-translate-y-full': scrollingDown,
+    'translate-y-0': scrollingUp,
+}">
 
     <div class="flex items-center gap-3">
-        <x-button type="button" class="md:hidden" icon-only variant="secondary" sr-text="Toggle dark mode" x-on:click="toggleTheme">
-        </x-button>
     </div>
 
-    <div class="flex items-center gap-3">
-        <x-button type="button" class="hidden md:inline-flex" icon-only variant="secondary" sr-text="Toggle dark mode" x-on:click="toggleTheme">
-        </x-button>
+    <div class="relative" x-data="{ open: false }" x-on:click.away="open = false" x-on:close.stop="open = false">
+        <div x-on:click="open = !open" class="cursor-pointer">
+            <button class="flex items-center p-2 text-sm font-medium text-gray-500 rounded-md transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-200">
+                <div>{{ Auth::user()->name }}</div>
 
-        <x-dropdown align="right" width="48">
-            <x-slot name="trigger">
-                <button class="flex items-center p-2 text-sm font-medium text-gray-500 rounded-md transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-200">
-                    <div>{{ Auth::user()->name }}</div>
+                <div class="ml-1">
+                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </button>
+        </div>
 
-                    <div class="ml-1">
-                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                </button>
-            </x-slot>
+        <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute z-50 mt-2 right-0 w-48 rounded-lg shadow-lg" style="display: none;" x-on:click="open = false">
+            <div class="bg-white dark:bg-dark-eval-2">
+                <!-- Dropdown Items -->
+                <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-dark-eval-3">Profile</a>
 
-            <x-slot name="content">
-                <!-- Profile -->
-                <x-dropdown-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-dropdown-link>
-
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-
-                    <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-dropdown-link>
+                    <button type="submit" class="block w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-red-100 hover:text-red-900 focus:outline-none focus:ring focus:ring-red-300 dark:text-red-400 dark:hover:bg-red-700 dark:hover:text-red-100">Log Out</button>
                 </form>
-            </x-slot>
-        </x-dropdown>
+            </div>
+        </div>
     </div>
+
 </nav>
 
 <!-- Mobile bottom bar -->
