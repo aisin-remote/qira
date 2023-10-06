e75624cd3ba06ba126d10356fc2fe3e76a313a0d
naufalamr17 <naufal.amr17@outlook.co.id>
2023-10-06 07:39:25 +0700

    Update: Change File Name in Project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -106,7 +106,7 @@ class ProjectController extends Controller
                             $originalFileName = uniqid() . '.' . $document->getClientOriginalExtension();
 
                             // Gabungkan dengan nilai $itemData['nama'] untuk membentuk path lengkap
-                            $documentPath = $document->storeAs('public/documents/', $project->pcr . '_' .$itemData['nama'] . '_' . $originalFileName);
+                            $documentPath = $document->storeAs('public/documents/', $project->pcr . '_' . $itemData['nama'] . '_' . $originalFileName);
 
                             $itemCheckProject->document = $documentPath;
                         } else {
@@ -163,7 +163,7 @@ class ProjectController extends Controller
                                 $originalFileName = uniqid() . '.' . $document->getClientOriginalExtension();
 
                                 // Gabungkan dengan nilai $itemData['nama'] untuk membentuk path lengkap
-                                $documentPath = $document->storeAs('public/documents/', $project->pcr . '_' .$itemData['nama'] . '_' . $originalFileName);
+                                $documentPath = $document->storeAs('public/documents/', $itemData['nama'] . '_' . $originalFileName);
 
                                 $item->document = $documentPath;
                             } else {
@@ -191,7 +191,7 @@ class ProjectController extends Controller
                             // Ambil nama asli file dokumen
                             $originalFileName = uniqid() . '.' . $document->getClientOriginalExtension();
                             // Gabungkan dengan nilai $itemData['nama'] untuk membentuk path lengkap
-                            $documentPath = $document->storeAs('public/documents/', $project->pcr . '_' .$itemData['nama'] . '_' . $originalFileName);
+                            $documentPath = $document->storeAs('public/documents/', $project->pcr . '_' . $itemData['nama'] . '_' . $originalFileName);
 
                             $newItem->document = $documentPath;
                         } else {
